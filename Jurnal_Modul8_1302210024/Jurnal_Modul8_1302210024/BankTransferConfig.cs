1c3fab974a297dcf97add4c2d2bf51a7510cc2b5
BlyatMan221 <119160448+HelloWorldYessir420@users.noreply.github.com>
2023-04-11 16:30:02 +0700

    aplikasi bank transfer dengan runtime configuration

---
+++ Jurnal_Modul8_1302210024/Jurnal_Modul8_1302210024/BankTransferConfig.cs
@@ -0,0 +1,73 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.IO;
+
+namespace Jurnal_Modul8_1302210024
+{
+    class BankTransferConfig
+    {
+        private JObject _config;
+
+        public BankTransferConfig()
+        {
+            string filePath = "bank_transfer_config.json";
+
+            if (!File.Exists(filePath))
+            {
+                // Set default values
+                _config = JObject.Parse(@"{
+                    'lang': 'en',
+                    'transfer': {
+                        'threshold': 25000000,
+                        'low_fee': 6500,
+                        'high_fee': 15000
+                    },
+                    'methods': [
+                        'RTO (real-time)',
+                        'SKN',
+                        'RTGS',
+                        'BI FAST'
+                    ],
+                    'confirmation': {
+                        'en': 'yes',
+                        'id': 'ya'
+                    }
+                }");
+            }
+            else
+            {
+                _config = JObject.Parse(File.ReadAllText(filePath));
+            }
+        }
+
+        public string GetLanguage()
+        {
+            return _config.Value<string>("lang");
+        }
+
+        public double GetTransferThreshold()
+        {
+            return _config.SelectToken("transfer.threshold").Value<double>();
+        }
+
+        public double GetLowFee()
+        {
+            return _config.SelectToken("transfer.LowFee").Value<double>(7500);
+        }
+
+        public double GetHighFee()
+        {
+            return _config.SelectToken("transfer.HighFee").Value<double>(20000);
+        }
+
+        public string[] GetMethods()
+        {
+            return _config.SelectToken("Methods").ToObject<string[]>();
+        }
+
+        public string GetConfirmation()
+        {
+            return _config.SelectToken($"Confirmation.{GetLanguage()}").Value<string>();
+        }
+    }
+}
---
+++ Jurnal_Modul8_1302210024/Jurnal_Modul8_1302210024/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+using Newtonsoft.Json.Linq;
+
+namespace Jurnal_Modul8_1302210024
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BankTransferConfig config = new BankTransferConfig();
+            Console.WriteLine(config.GetLanguage() == "en" ? "Please insert the amount of money to transfer:" : "Masukkan jumlah uang yang akan di-transfer:");
+
+            double amount = double.Parse(Console.ReadLine());
+            double transferFee = amount <= config.GetTransferThreshold() ? config.GetLowFee() : config.GetHighFee();
+            double totalAmount = amount + transferFee;
+
+            Console.WriteLine(config.GetLanguage() == "en" ? $"Transfer fee = {transferFee}" : $"Biaya transfer = {transferFee}");
+            Console.WriteLine(config.GetLanguage() == "en" ? $"Total amount = {totalAmount}" : $"Total biaya = {totalAmount}");
+
+            Console.WriteLine(config.GetLanguage() == "en" ? "Select transfer method:" : "Pilih metode transfer:");
+            int i = 1;
+            foreach (string method in config.GetMethods())
+            {
+                Console.WriteLine($"{i++}. {method}");
+            }
+
+            Console.WriteLine(config.GetLanguage() == "en" ? $"Please type \"{config.GetConfirmation()}\" to confirm the transaction:" : $"Ketik \"{config.GetConfirmation()}\" untuk mengkonfirmasi transaksi:");
+            string confirmation = Console.ReadLine();
+
+            if (confirmation == config.GetConfirmation())
+            {
+                Console.WriteLine(config.GetLanguage() == "en" ? "The transfer is completed" : "Proses transfer berhasil");
+            }
+            else
+            {
+                Console.WriteLine(config.GetLanguage() == "en" ? "Transfer is cancelled" : "Transfer dibatalkan");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
